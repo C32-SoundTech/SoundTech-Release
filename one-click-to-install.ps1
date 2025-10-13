bb0598b27a2f5b4c5c116d7d9a16901ba5653cd5
丁炜宸 <2312190131@pop.zjgsu.edu.cn>
2025-10-13 14:01:47 +0800

    feat: 添加一键安装SoundTech的Windows脚本
    添加PowerShell脚本实现SoundTech在Windows系统的一键安装，包括下载解压程序、配置Python环境及安装依赖

---
+++ one-click-to-install.ps1
@@ -0,0 +1,33 @@
+Clear-Host
+Write-Host "Downloading SoundTech V1.0.0 Windows version..."
+curl https://gitee.com/dingdust/SoundTech-Release/raw/main/V1.0.0-Windows.zip -o .\one-click-to-install.zip
+New-Item -Path .\SoundTech -ItemType Directory
+Expand-Archive -Path .\one-click-to-install.zip -DestinationPath .\SoundTech
+Remove-Item -Path .\one-click-to-install.zip
+Set-Location .\SoundTech
+
+Clear-Host
+Write-Host "Downloading Python 3.14.0 64-bit embedded version..."
+curl https://gitee.com/dingdust/SoundTech-Release/raw/main/python-3.14.0-embed-amd64.zip -o .\python314-embed.zip
+
+Clear-Host
+New-Item -Path .\python314-embed -ItemType Directory
+Expand-Archive -Path .\python314-embed.zip -DestinationPath .\python314-embed
+Remove-Item -Path .\python314-embed.zip
+Set-Location .\python314-embed
+Set-Content -Path ".\python314._pth" -Encoding ascii -Value @"
+python314.zip
+.
+..
+import site
+"@
+
+Clear-Host
+Write-Host "Downloading necessary requirements..."
+curl https://gitee.com/dingdust/SoundTech-Release/raw/main/get-pip.py -o .\get-pip.py 
+.\python.exe .\get-pip.py --no-warn-script-location
+.\python.exe -m pip install -r ..\requirements.txt --index-url https://mirrors.aliyun.com/pypi/simple/ --no-warn-script-location
+Set-Location ..
+
+Clear-Host
+.\python314-embed\python.exe .\app.py
